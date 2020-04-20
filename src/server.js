57c3e089120872282423b252d1440742c826b07a
Kapakahi <rubbishman@gmail.com>
2020-04-20 16:17:51 +0900

    Finally getting the ball rolling

--- src/server.js
+++ src/server.js
@@ -1,6 +1,18 @@
+const express = require("express");
 const pokeData = require("./data");
 
 const setupServer = () => {
+  const app = express();
+
+  app.get("/api/pokemon", (request, response) => {
+    response.send(pokeData.pokemon);
+  });
+
+  return app;
+
+  // const number = request.query.number;
+  //   `https://pokeapi.co/api/v2/pokemon?limit=${n}`
+  // `pokeData.pokemon.json?limit=${n - 1}`
   /**
    * Create, set up and return your express server, split things into separate files if it becomes too long!
    */
--- test/_pokemonApi.js
+++ test/_pokemonApi.js
@@ -2,16 +2,34 @@ const chai = require("chai");
 const chaiHttp = require("chai-http");
 chai.use(chaiHttp);
 const { setupServer } = require("../src/server");
+chai.should();
+const pokeData = require("../src/data");
 
-/*
- * This sprint you will have to create all tests yourself, TDD style.
- * For this you will want to get familiar with chai-http https://www.chaijs.com/plugins/chai-http/
- * The same kind of structure that you encountered in lecture.express will be provided here.
- */
+//  * This sprint you will have to create all tests yourself, TDD style.
+//  * For this you will want to get familiar with chai-http https://www.chaijs.com/plugins/chai-http/
+//  * The same kind of structure that you encountered in lecture.express will be provided here.
+
 const server = setupServer();
 describe("Pokemon API Server", () => {
   let request;
   beforeEach(() => {
     request = chai.request(server);
   });
+  //Making a get request to /greet .query is used to pass data into a get requesst. /greet?name=Mia
+
+  describe("GET /api/pokemon - should return a full list ", () => {
+    it("should return a full list of pokemon", async () => {
+      const res = await request.get("/api/pokemon"); //.query({ n : 1 }); //maybe ../data/pokemon??
+      res.should.be.json;
+      JSON.parse(res.text).should.eql(pokeData.pokemon);
+    });
+  });
+
+  // describe("GET api/pokemon - should return a full list ", () => {
+  //   it("should be able to limit the number of pokemon listed", async () => {
+  //     const res = await request.get("api/pokemon") //.query({ n : 1 }); //maybe ../data/pokemon??
+  //     res.should.be.json;
+  //     res.json.should.equal(`pokeData.pokemon[0]`);
+  //   });
+  // });
 });
